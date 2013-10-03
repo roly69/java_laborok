8bcf05896a5f2027faeccbf3dfdb78157ad8671b
roly69 <roly69@kmf.uz.ua>
2013-10-03 09:28:13 +0200

    Create PróbaApp.java
    Próba

---
+++ PróbaApp.java
@@ -0,0 +1,22 @@
+{
+"type" : "FeatureCollection",
+"features" :[
+   {
+     "type" : "Features",
+     "properties" : {},
+     "geometry" :{
+       "type": "Point",
+       "coordinates":[201.9287109375 , 21.3]
+    }
+  },
+  {
+   "type": "Feature",
+   "properties":{},
+   "geometry":{
+    "type":"Point",
+    "coordinates":[798.5,36.5]
+    
+    }
+  }  
+  ]
+}  
